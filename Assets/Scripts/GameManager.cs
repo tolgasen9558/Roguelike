c1058e429302c76bb47fc404a3ff3af93c99300c
Tolga Şen <tolgasen9558@gmail.com>
2017-05-10 23:20:19 +0300

    Set UI elements of the game (Food text, Day text, Level Canvas)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public float levelStartDelay = 2f;
 	public float turnDelay = 0.1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
 
-	private int level = 3;
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 0;
 	private List<Enemy> enemies;
 	private bool enemiesMoving;
+	private bool doingSetup;
 
 	void Awake () {
 		if (instance == null){
@@ -25,21 +30,50 @@ public class GameManager : MonoBehaviour {
 		DontDestroyOnLoad(gameObject);
 		enemies = new List<Enemy>();
 		boardScript = GetComponent<BoardManager>();
+	}
+
+	private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode){
+		level++;
 		InitGame();
 	}
 
+	void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
 	void InitGame(){
+		doingSetup = true;
+
+		levelImage = GameObject.Find("Level Image");
+		levelText = GameObject.Find("Level Text").GetComponent<Text>();
+		levelText.text = "Day " + level;
+		levelImage.SetActive(true);
+		Invoke("HideLevelImage", levelStartDelay);
+
 		enemies.Clear();
 		boardScript.SetupScene(level);
 	}
 
+	private void HideLevelImage(){
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
 	public void GameOver(){
+		levelText.text = "After " + level + " days, you starved.";
+		levelImage.SetActive(true);
 		enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(playersTurn || enemiesMoving){
+		if(playersTurn || enemiesMoving || doingSetup){
 			return;
 		}
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -9,6 +10,7 @@ public class Player : MovingObject {
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
+	public Text foodText;
 
 	private Animator animator;
 	private int food;
@@ -17,6 +19,7 @@ public class Player : MovingObject {
 	protected override void Start () {
 		animator = GetComponent<Animator>();
 		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
 		base.Start();
 	}
 
@@ -46,7 +49,7 @@ public class Player : MovingObject {
 
 	protected override void AttemptMove <T> (int xDir, int yDir){
 		food--;
-
+		foodText.text = "Food: " + food;
 		base.AttemptMove<T>(xDir,yDir);
 
 		RaycastHit2D hit;
@@ -63,10 +66,12 @@ public class Player : MovingObject {
 		}
 		else if(other.tag == "Food"){
 			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + "Food: " + food;
 			other.gameObject.SetActive(false);
 		}
 		else if(other.tag == "Soda"){
 			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + "Food: " + food;
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -84,6 +89,7 @@ public class Player : MovingObject {
 	public void LoseFood(int loss){
 		animator.SetTrigger("playerHit");
 		food -= loss;
+		foodText.text = "-" + loss + "Food: " + food;
 		CheckIfGameOver();
 	}
 
